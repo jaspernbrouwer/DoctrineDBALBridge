c40a814759d7bf96dcef9ac2b52d7b9f687b3c6a
Jasper N. Brouwer <jasper@nerdsweide.nl>
2014-12-23 14:21:05 +0100

    Initial commit (WrapsNextCommandInTransaction)

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
 DoctrineDBALBridge
 ==================
 
-Bridge for using CommandBus and EventBus with Doctrine DBAL
+A bridge for using [SimpleBus](https://github.com/SimpleBus) with [Doctrine DBAL](http://docs.doctrine-project.org/projects/doctrine-dbal/en/latest/). 
+
+By [Jasper N. Brouwer](https://github.com/jaspernbrouwer)
+
+## Installation
+
+Using Composer:
+
+    composer require jaspernbrouwer/doctrine-dbal-bridge
+
+## Usage
+
+1. Set up a [command bus](https://github.com/SimpleBus/CommandBus):
+
+    ```php
+    $commandBus = ...;
+    ```
+
+2. Set up a Doctrine DBAL connection:
+
+    ```php
+    $connection = ...;
+    ```
+
+3. Wrap the existing command bus in order to handle your commands inside a database transaction: 
+
+    ```php
+    use JNB\DoctrineDBALBridge\CommandBus\WrapsNextCommandInTransaction;
+    
+    $transactionalCommandBus = new WrapsNextCommandInTransaction($connection);
+    $transactionalCommandBus->setNext($commandBus);
+    ```
---
+++ src/CommandBus/WrapsNextCommandInTransaction.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ * For more information, please view the LICENSE file that was distributed with
+ * this source code.
+ */
+
+namespace JNB\DoctrineDBALBridge\CommandBus;
+
+use Doctrine\DBAL\Driver\Connection;
+use SimpleBus\Command\Bus\CommandBus;
+use SimpleBus\Command\Bus\RemembersNext;
+use SimpleBus\Command\Command;
+
+/**
+ * Class WrapsNextCommandInTransaction
+ *
+ * @license https://github.com/jaspernbrouwer/DoctrineDBALBridge/blob/master/LICENSE MIT
+ * @author  Jasper N. Brouwer
+ */
+class WrapsNextCommandInTransaction implements CommandBus
+{
+    use RemembersNext;
+
+    /**
+     * @var Connection
+     */
+    private $connection;
+
+    /**
+     * @param Connection $connection
+     */
+    public function __construct(Connection $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle(Command $command)
+    {
+        $this->connection->beginTransaction();
+
+        try {
+            $this->next($command);
+
+            $this->connection->commit();
+        } catch (\Exception $e) {
+            $this->connection->rollback();
+
+            throw $e;
+        }
+    }
+}
